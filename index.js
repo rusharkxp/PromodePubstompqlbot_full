474f2ba9fd7f9a0abb244ea39549182b4bb39722
Shark <37933820+rusharkxp@users.noreply.github.com>
2019-12-11 17:27:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -30,38 +30,7 @@ app.use(bot.webhookCallback('/bot'));
 bot.telegram.setWebhook('https://telegraftelegrambot.herokuapp.com/bot');
 
 bot.hears(/\/start/,(ctx) => {
-    const telegramID = ctx.message.hasOwnProperty('chat') ? ctx.message.chat.id : ctx.message.from.id
-    const user = new User({
-        id:telegramID,
-        isAdmin:false,
-        username:ctx.update.message.from.username
-    })
-    const isNew = helper.isNewUser(telegramID).then(p =>{
-        if(p){
-            user.save().then(() =>{
-                return ctx.reply(`Добро пожаловать в наш бот!`,Extra.markup((markup) =>{
-                    return markup.resize()
-                        .keyboard(kb.main_menu)
-                }))
-
-            })
-        }
-        if(!p){
-            User.find({id:telegramID,isAdmin:true}).then((u) =>{
-                if(u.length !== 0){
-                    ctx.reply(`Добро пожаловать в наш бот!`, Extra.markup((markup) => {
-                        markup.resize()
-                        return markup.keyboard(kb.main_menuSecret)
-                    }))
-                }else{
-                    ctx.reply(`Добро пожаловать в наш бот!`,Extra.markup((markup) =>{
-                        markup.resize()
-                        return markup.keyboard(kb.main_menu)
-                    }))
-                }
-            })
-        }
-    }).catch(error => ctx.reply(`Что-то пошло не так\n${error}`))
+    ctx.reply('hello')
 })
 
 const port = process.env.PORT || 3000;
